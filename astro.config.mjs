fcb11755c9b231253a38c1de3fa3b757384ba910
Samuel Viana da Silva <samuelviana2626@gmail.com>
2025-08-17 13:40:01 -0400

    adding project

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://samuel-aka-viana.github.io/', // se for user.github.io use apenas 'https://<USER>.github.io/'
-    base: '/',                          // se for user.github.io use '/'
+    site: 'https://devopsmanaus.github.io/', // se for user.github.io use apenas 'https://<USER>.github.io/'
+    base: '/',
     output: 'static'
 });
