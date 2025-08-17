e1a8ab7339e5cff244b83842378b517da73563b1
Samuel Viana da Silva <samuelviana2626@gmail.com>
2025-08-17 13:44:52 -0400

    feature(10): adding project to astro framework

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://devopsmanaus.github.io/', // se for user.github.io use apenas 'https://<USER>.github.io/'
+    site: 'https://devopsmanaus.github.io/',
     base: '/',
     output: 'static'
 });
